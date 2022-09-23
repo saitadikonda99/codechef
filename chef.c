9c8e4e4c6704931baba6318f254440fe648b6052
saitadikonda99 <saiitadikonda7@gmail.com>
2022-09-23 14:22:33 +0530

    addition of two integers

---
+++ chef.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main() {
+                                 //Read the number of test cases.
+int T;
+scanf("%d",&T);
+while(T--) { 
+                                //read the input a,b
+int a,b;
+scanf("%d %d",&a,&b);
+int ans=a+b;
+printf("%d\n",ans);            //compute the ans
+}
+return 0;
+}
