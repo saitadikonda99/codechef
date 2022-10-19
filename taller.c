f79393ec18b58d9acbe510fad8721928f649eef2
saitadikonda99 <saiitadikonda7@gmail.com>
2022-10-19 13:33:19 +0530

    Who is taller

---
+++ taller.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+ int t;
+ scanf("%d",&t);
+ while(t--) {
+     int x;
+     scanf("%d",&x);
+     int y;
+     scanf("%d",&y);
+     if( x > y ) 
+         printf("A\n");
+         else 
+         printf("B\n");
+     }
+	return 0;
+}
+
+
