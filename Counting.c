bbe6261e372bbfeb3a92fd221503890ef41ca300
saitadikonda99 <saiitadikonda7@gmail.com>
2022-10-20 13:30:47 +0530

    Counting Words

---
+++ Counting.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    int t;
+    scanf("%d",&t);
+    while(t--) {
+        int n,m;
+        scanf("%d%d",&n,&m);
+        printf("%d\n",n*m);
+    }
+    
+    
+    
+	return 0;
+	
+}
+
+
