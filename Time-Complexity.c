ad9370799c56cf01b717887550b5a87407154baf
saitadikonda99 <saiitadikonda7@gmail.com>
2022-11-13 21:19:34 +0530

    code-chef november contest

---
+++ Time-Complexity.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int z;
+	scanf("%d",&z);
+    while( z-- ) {
+	int a,b;
+	scanf("%d %d", &a,&b);
+    if( a>b ) 
+	printf("YES\n");
+    else
+  	printf("NO\n");
+	      }
+	return 0;
+	}
+
+
