42e60df176e5a1ff35c9770505175b8c0c81b95a
saitadikonda99 <saiitadikonda7@gmail.com>
2022-11-24 23:44:16 +0530

    SQUAT PROBLEM IN CODE CHEF

---
+++ Squat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 	int main() {
+ 	    int k;
+ 	    scanf( "%d",&k);
+ 	    
+ 	while(k--) {
+ 	    int s;
+ 	    scanf("%d",&s);
+ 	    printf("%d\n",s*15);
+ 	}
+ 	
+ 	return 0;
+}
+
+
