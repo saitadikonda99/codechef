78d2331ca556e6be9ada2492bba118d5086e4864
saitadikonda99 <saiitadikonda7@gmail.com>
2022-11-26 23:29:01 +0530

    code chef problem

---
+++ Get-Subscription.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	 int k;
+	 scanf("%d",&k);
+
+while(k--) {
+    int s;
+    scanf("%d",&s);
+    
+if( s > 30 ) 
+    printf("YES\n");
+    else 
+    printf("NO\n");
+    
+}
+
+	return 0;
+}
+
+
