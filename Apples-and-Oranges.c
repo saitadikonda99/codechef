1942d25c66a13ce650dcdba9336806c0dadd462b
saitadikonda99 <saiitadikonda7@gmail.com>
2022-11-27 14:10:07 +0530

    code chef problem

---
+++ Apples-and-Oranges.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+            
+                int c,s,t;
+                scanf("%d%d%d",&c,&s,&t);
+                if ( s+t <= c )
+                    printf("YES\n");
+                    else 
+                        printf("NO\n");
+           
+	return 0;
+}
+
+
