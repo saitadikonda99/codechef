4bf67117c4f6300ce54fe934f9bafb841e367843
saitadikonda99 <saiitadikonda7@gmail.com>
2022-11-29 18:05:41 +0530

    just for fun

---
+++ Volume-Control.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void) {
+	 int k;
+	 scanf("%d",&k);
+while( k-- ) {
+     int x,y;
+     scanf("%d%d",&x,&y);
+     if( x>y) {
+         int press = x-y;
+         printf("%d\n",press);
+           }
+           else {
+               int press = y -x;
+                printf("%d\n",press);
+           }
+}
+
+          return 0;
+}
+
+
