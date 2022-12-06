db7d73d4f4c604bc6b7dd3bc61b7befe65039c29
saitadikonda99 <saiitadikonda7@gmail.com>
2022-12-06 23:21:20 +0530

    code chef problem

--- Age-Limit.c
+++ Age-Limit.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void) {
-       int s;
-       scanf("%d",&s);
-       
-while( s-- ) {
-    int k,r,d;
-    scanf("%d%d%d",&k,&r,&d); 
-        if ( d < r && d >=k ) 
-            printf("YES\n");
-            else 
-                printf("NO\n");
-    }
+	 int k;
+	 scanf ("%d",&k);
+
+while(k-- ) {  
+        int c,o,m;
+        scanf("%d%d%d",&c,&o,&m);
+           if(c <= m && m<o )
+                printf("YES\n");
+                else 
+                    printf("NO\n");
+}
 	return 0;
 }
 
